3191ee84d4fa11cad798a827bb001897a744ed00
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-05-09 10:40:05 -0400

    updated app.js

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
 const express = require("express")
 const cors = require("cors");
 const groceriesControllers = require("./controllers/groceriesControllers.js");
-
-
 //CONFIG
 const app = express();
-
 
 // MIDDLEWARE
 app.use(cors());
 app.use(express.json());//parses incoming json request
-
 
 // ROUTES
 app.get("/", (req, res)=> {
