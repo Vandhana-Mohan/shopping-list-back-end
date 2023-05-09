9e45e393332d9de155df173b1c8e74916c22cfaf
touhami-ben <brooklyn.ben76@gmail.com>
2023-05-09 00:35:49 -0400

    create controllers queries and db

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const cors = require("cors");
+const groceriesControllers = require("./controllers/groceriesControllers.js");
+
+
+//CONFIG
+const app = express();
+
+
+// MIDDLEWARE
+app.use(cors());
+app.use(express.json());//parses incoming json request
+
+
+// ROUTES
+app.get("/", (req, res)=> {
+    res.send("Welcome to shopping cart")
+});
+app.use("/groceries", groceriesControllers);
+app.get("*", (req, res) => {
+    res.status(404).send("Page not found")
+});
+
+module.exports = app;
---
+++ controllers/groceriesControllers.js
@@ -0,0 +1,16 @@
+const express = require("express");
+ const groceries = express.Router();
+ const { getAllGroceries} =- require("../queries/groceries.js");
+
+
+ // INDEX
+ groceries.get("/", async (req, res) => {
+    const allGroceries = await getAllGroceries();
+    if(!allGroceries.error){
+        res.status(200).json(allGroceries);
+    }else{
+        res.status(500).json({ error: "server error" });
+    }
+});
+
+module.exports = groceries;
