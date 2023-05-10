2029b02c8cd14d374ebf14f184ccaa7beb844cca
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-05-10 04:18:26 -0400

    fixed the error in image url link

--- db/seed.sql
+++ db/seed.sql
@@ -1,4 +1,6 @@
 \c groceries_dev;
 
  INSERT INTO groceries (name, category, image_url, description, price, quantity, unit, is_organic) VALUES
-  ('Milk', 'Dairy', 'https://www.google.com/aclk?sa=l&ai=DChcSEwiUht-g_On-AhU9_-MHHWfnAB4YABAFGgJ5bQ&sig=AOD64_1-JXnJ70_at7BrnnZepti-WuZXug&adurl&ctype=46&ved=2ahUKEwi1mdGg_On-AhVeElkFHVJsBa0Qvhd6BQgBEIYB', 'Low-fat-Milk', 4.99, 1, 'Gallon', false);
+  ('Milk', 'Dairy', 'https://www.foodsafetynews.com/files/2021/06/illustration-milk-in-glass.jpg', 'Low-fat-Milk', 4.99, 1, 'Gallon', false);
+
+  
