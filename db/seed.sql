4c658f00af7ea7597e6f0668303d9270285cffdf
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-05-10 14:02:38 -0400

    update seed

--- db/seed.sql
+++ db/seed.sql
@@ -2,9 +2,8 @@
 
  INSERT INTO groceries (name, category, image_url, description, price, quantity, unit, is_organic) VALUES
   ('Milk', 'Dairy', 'https://www.foodsafetynews.com/files/2021/06/illustration-milk-in-glass.jpg', 'Low-fat-Milk', 4.99, 1, 'Gallon', false),
-('Apple', 'Fruits', 'https://www.foodsafetynews.com/files/2021/09/wax-apple-660x374.png', 'fresh',  2.99, 2, 'pound', false),
-('Potatoes', 'Vegetables', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRN6TKHj0G_rMVbSRmTrhKhCEa22c2qy4lVzjCayANR2g&s', 'tuber-round-oval', 2.29, 10, 'pound', true),
-('Bread Loaf', 'Bakery', 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQ-Tq7ywHXRBUVW9lOBYw0XuYTj_WsOzlpTnc6ZKzQ5vTQAE7xA1Uk38B-DCWw3HQEVU1t9RK3j', '21 Whole Grains', 5.99, 1, 'oz', true),
-('Beef Steak', 'Meat', 'https://assets.kansascitysteaks.com/dyn-images/pdp_hero/TBone_Prime_Raw_SU07-b3250c1962c455e6e20b6debcf6d6678.jpg', 'USDA Prime T_bone Steak', 7.99, 1, 'pound', false),
-('Pepsi', 'Beverages', 'https://m.media-amazon.com/images/I/41b4770tmqL._PIbundle-24,TopRight,0,0_SX273SY500SH20_.jpg', 'Zero-Sugar', 27, 24, 'Oz', false);
-  
+  ('Apple', 'Fruits', 'https://www.foodsafetynews.com/files/2021/09/wax-apple-660x374.png', 'fresh',  2.99, 2, 'pound', false),
+  ('Potatoes', 'Vegetables', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRN6TKHj0G_rMVbSRmTrhKhCEa22c2qy4lVzjCayANR2g&s', 'tuber-round-oval', 2.29, 10, 'pound', true),
+  ('Bread Loaf', 'Bakery', 'https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcQ-Tq7ywHXRBUVW9lOBYw0XuYTj_WsOzlpTnc6ZKzQ5vTQAE7xA1Uk38B-DCWw3HQEVU1t9RK3j', '21 Whole Grains', 5.99, 1, 'oz', true),
+  ('Beef Steak', 'Meat', 'https://assets.kansascitysteaks.com/dyn-images/pdp_hero/TBone_Prime_Raw_SU07-b3250c1962c455e6e20b6debcf6d6678.jpg', 'USDA Prime T_bone Steak', 7.99, 1, 'pound', false),
+  ('Pepsi', 'Beverages', 'https://m.media-amazon.com/images/I/41b4770tmqL._PIbundle-24,TopRight,0,0_SX273SY500SH20_.jpg', 'Zero-Sugar', 27, 24, 'oz', false);
