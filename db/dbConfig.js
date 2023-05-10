30316630937d5baed3e1bf1cb5d0cb8735b2979e
Vandhana-Mohan <mohan.vandhana@gmail.com>
2023-05-10 13:10:10 -0400

    update

--- db/dbConfig.js
+++ db/dbConfig.js
@@ -6,15 +6,13 @@ const cn = DATABASE_URL
   ? {
       connectionString: DATABASE_URL,
       max: 30,
-      
-      }
-    
-:{
-  host: process.env.PG_HOST,
-  port: process.env.PG_PORT,
-  database: process.env.PG_DATABASE,
-  user: process.env.PG_USER,
-};
+    }
+  : {
+      host: process.env.PG_HOST,
+      port: process.env.PG_PORT,
+      database: process.env.PG_DATABASE,
+      user: process.env.PG_USER,
+    };
 
 const db = pgp(cn);
 
